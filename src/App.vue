cf5cf7849072d27ba08ca6d2134542c442386337
boom <564296557@qq.com>
2019-07-09 17:29:58 +0800

    登录界面

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <mt-header fixed :title="headerTitle">
+      <router-link to="/" slot="left">
+        <mt-button icon="back"></mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      headerTitle:'加减医学'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width:100%;
+  height:100%;
+}
+*{
+  margin:0;
+  padding:0;
+}
+html,body{
+  width:100%;
+  height:100%;
+  background: #fafafa
+}
+ul{list-style:none}
+a{text-decoration: none;}
+img{vertical-align:top;border:none}
+
+.clearf:after,
+.clearf:before{
+    content:"";
+    display:block;
+    clear:both;
+}
+</style>
---
+++ src/view/login/login.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="login-concent">
+    <div class="login-concent-from">
+      <div class="login-concent-img">
+        <img src="@/view/login/lib/logo.png">
+      </div>
+      <div class="wLogin">
+        <mt-radio
+          v-model="value"
+          :options="options">
+        </mt-radio>
+          <mt-field label="手机号码" type="text" v-model="email" v-if="value==1"></mt-field>
+          <mt-field label="账号" type="text" v-model="user" v-if="value==0"></mt-field>
+          <mt-field label="验证码" type="text" v-model="user"></mt-field>
+          <mt-field label="密码" type="text" v-model="password" v-if="value==1"></mt-field>
+          <mt-field label="短信验证码" type="text" v-model="password" v-if="value==0">
+            <!-- <mt-button type="default" size="small">获取验证码</mt-button> -->
+            <short :shortData='shortData'></short>
+          </mt-field>
+          <div class="submitBox">
+              <mt-button type="primary" size="large" @click="submitLogin">确 定</mt-button>
+          </div>
+
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+import Short from '../../components/wShort'
+
+export default {
+    name:'login',
+    data() {
+      return {
+        shortData:{
+          time:10,
+          url:'http://www.baidu.com'
+        },
+        email:'',
+        user:'',
+        password:'',
+        value:'0',
+        options:[
+          {
+            label: '个人客户',
+            value: '0',
+          },
+          {
+            label: '医疗机构',
+            value: '1'
+          }
+        ]
+      }
+    },
+    activated() {
+    },
+    methods:{
+      submitLogin(){
+        this.$axios.get('http://www.wuts66.com')
+        .then((res)=>{
+          console.log(res)
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+      }
+    },
+    components:{
+      Short
+    }
+}
+</script>
+<style lang="scss" rel="stylesheet/scss">
+  .mint-radiolist{
+    clear: both;
+    overflow: hidden;
+    a{
+      float: left;
+      width: 50%;
+      text-align: center;
+      background-color: transparent;
+    }
+  }
+  .login-concent{
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+  }
+  .wLogin .mint-cell{
+    border-bottom: 1px solid #eee;
+    color: #888;
+  }
+  .login-concent-from{
+    width: 100%;
+    .login-concent-img{
+      width: 200px;
+      margin: 0 auto;
+      padding-bottom: 35px;
+      img{
+        width: 100%;
+      }
+    }
+  }
+  .submitBox{
+    padding: 15px;
+  }
+</style>
+
+
