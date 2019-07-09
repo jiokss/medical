84484fa93b251bcece4d2eb979995f07a89373b0
jiokss <372275526@qq.com>
2019-07-09 15:46:09 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width:100%;
+  height:100%;
+}
+*{
+  margin:0;
+  padding:0;
+}
+html,body{
+  width:100%;
+  height:100%;
+  background: #fafafa
+}
+ul{list-style:none}
+a{text-decoration: none;}
+img{vertical-align:top;border:none}
+
+.clearf:after,
+.clearf:before{
+    content:"";
+    display:block;
+    clear:both;
+}
+</style>
---
+++ src/view/login/login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login-concent">
+    <div class="login-concent-from">
+      <div class="login-concent-img">
+        <img src="@/view/login/lib/logo.png">
+      </div>
+      <div class="wLogin">
+          <mt-field label="手机号码" type="text" v-model="email"></mt-field>
+          <mt-field label="验证码" type="text" v-model="user"></mt-field>
+          <mt-field label="短信验证码" type="text" v-model="password">
+            <mt-button type="default" size="small">获取验证码</mt-button>
+          </mt-field>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+    name:'login',
+    data() {
+      return {
+        email:'',
+        user:'',
+        password:'',
+      }
+    },
+    activated() {
+    }
+}
+</script>
+<style lang="scss" rel="stylesheet/scss">
+  .login-concent{
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+  }
+  .wLogin .mint-cell{
+    border-bottom: 1px solid #eee;
+    color: #888;
+  }
+  .login-concent-from{
+    width: 100%;
+    .login-concent-img{
+      width: 200px;
+      margin: 0 auto;
+      padding-bottom: 35px;
+      img{
+        width: 100%;
+      }
+    }
+  }
+</style>
+
+
